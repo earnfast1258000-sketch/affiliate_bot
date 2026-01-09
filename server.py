0c9f37d1eb99e076f0f5e3966d0f2d1c279a139c
earnfast1258000-sketch <earnfast1258000@gmail.com>
2026-01-10 01:23:30 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot server running", 200
+
 @app.route("/postback")
 def postback():
     return "postback working", 200
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
